dfded70cbc30dd52e6aba0b59eac38a544c54c86
Neskens121 <nenadb92@yahoo.com>
2018-03-11 22:14:29 +0100

    initial commit

---
+++ quiz.php
@@ -0,0 +1,119 @@
+<?php 
+	var_dump($_POST);
+
+	$quizResult = 0;
+	$maxNumberOfQuestions = 5;
+	$currentQuestionNumber = $_POST ? $_POST['questionNumber'] : NULL;
+	if($_POST){
+		session_start();
+		
+
+		if(isset($_POST['logout'])){
+			$_SESSION = array();
+			if(isset($_COOKIE[session_name()])){
+				setcookie(session_name(), '', time() -86400, '/');
+			}
+			session_destroy();
+			header('Location: http://localhost/quiz.php');
+		}
+		require 'quizQuestions.php';
+		if(isset($_POST['startBtn'])){
+			$_SESSION['questionIndexArr'] = array_rand( $questions, $maxNumberOfQuestions );
+			shuffle($_SESSION['questionIndexArr']);
+			var_dump($_SESSION);
+			foreach ($_SESSION['questionIndexArr'] as $key => $value) {
+				$tempQuestionArr[] = $questions[$value];
+			}
+			$_SESSION['userAnswerArr'] = array();
+		}
+		if(isset($_POST['questionNumber'])) {
+			$tempQuestionArr = [];
+			foreach ($_SESSION['questionIndexArr'] as $key => $value) {
+				$tempQuestionArr[] = $questions[$value];
+
+			}
+			var_dump($tempQuestionArr);
+			if(isset($_POST['question'])){
+				$_SESSION['userAnswerArr'][$currentQuestionNumber-1] = array("questionIndex"=>"{$_SESSION['questionIndexArr'][$currentQuestionNumber-1]}", "indexOfAnswer"=>"{$_POST['question']}");
+				if($_POST['question'] == ($tempQuestionArr[$currentQuestionNumber-1]['indexOfCorrectAnswer'])){
+					var_dump($_SESSION);
+					$_SESSION['result'] = !isset($_SESSION['result']) ? 1 : $_SESSION['result']+1;
+					$quizResult = $_SESSION['result'] * 1;
+					//setcookie('result',$quizResult);
+					echo $quizResult . '<br>';
+					echo calculateScore($tempQuestionArr, $_SESSION['userAnswerArr']) . '<br>';
+				}
+			}
+		}
+	}
+	function calculateScore($questionsArr, $userInfoArr){
+		$score = 0;
+		for($i = 0; $i < count($userInfoArr); $i++){
+			if($questionsArr[$i]['indexOfCorrectAnswer'] == $userInfoArr[$i]['indexOfAnswer']){$score++;}
+		}
+		return $score;
+	}
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Quiz</title>
+	<style type="text/css">
+	ul {
+		list-style-type: none;
+		padding: 0;
+		margin: 0;
+	}
+	</style>
+</head>
+<body>
+	<?php if(!$_POST){ ?>
+	<h1>Welcome to our Quiz</h1>
+	<form action="" method="POST">
+		<input type="hidden" id="questionNumber" name="questionNumber" value="-1">
+		<input type="submit" name="startBtn" id="startBtn" value="startBtn">
+	</form>
+	<?php } elseif (isset($_POST['startBtn']) || $currentQuestionNumber < $maxNumberOfQuestions) { ?> 
+		<h3>Question Number: <?php echo ($currentQuestionNumber + 1)?></h3>
+		<p>
+			<?php echo  $tempQuestionArr[$currentQuestionNumber]['question']?>		
+		</p>
+		<form action="" method="POST">
+			<ul>
+				<?php foreach ($tempQuestionArr[$currentQuestionNumber]['potentialAnswers'] as $key => $value) {
+					echo "<li><input type='radio' name='question' value=$key>" . $value . "</li>";
+				} ?>		
+			</ul>
+			<input type="hidden" id="questionNumber" name="questionNumber" value=<?php echo $_POST['questionNumber']; ?>>
+			<input type='submit' name='nextQuestionBtn' id='nextQuestionBtn' value='Next Question'/>
+		</form>
+	<?php } else { ?>
+		<h3>Quiz is over</h3>
+		<h4>You scored <?php echo calculateScore($tempQuestionArr, $_SESSION['userAnswerArr']) ?> points</h4>
+		<form action="" method="POST">
+			<input name="logout" type="submit" id="logout" value="Start Quiz Again">
+		</form>
+	<?php } ?> 
+
+	<script type="text/javascript">
+		var startBtn = document.getElementById('startBtn');
+		var nextQuestionBtn = document.getElementById('nextQuestionBtn');
+		var questionNumber = document.getElementById('questionNumber');
+		
+		if(startBtn){startBtn.addEventListener('click', changeQuestionNumber(1), false);}
+		if(nextQuestionBtn){nextQuestionBtn.addEventListener('click', changeQuestionNumber(1), false);}
+
+
+		function changeQuestionNumber(val){
+			return function(){
+				//alert(val);
+				questionNumber.value = questionNumber.value * 1 + val;
+				//document.cookie = "result=0";
+				//alert(questionNumber);
+			};
+		}
+	</script>
+</body>
+</html>
---
+++ quizQuestions.php
@@ -0,0 +1,46 @@
+<?php 
+
+$questions = array(
+	array(
+		'question' => 'Pored toga što je osnovao fond iz kojeg će se svake godine dodijeljivati nagrade u više kategorija (Nobolova nagrada), Alfred Nobel je poznat i kao čovjek koji je izumio i patentirao',
+		'potentialAnswers' => ['Dinamit', 'Aspirin', 'Plastiku', 'Hemijsku olovku'],
+		'indexOfCorrectAnswer' => '0',
+		'descriptionOfCorrectAnswer' => 'Alfred Nobel je bio švedski hemičar, inovator, dramski pisac i donator. Na osnovama nitroglicerina Nobel je izumio i patentirao dinamit 1867. godine. Alfred Nobel je u svom testamentu donirao dio svog novca Švedskoj državi i osnovao fond iz kojeg će se svake godine dodijeljivati nagrade u više kategorija. Nobel je bio zaprepašten i šokiran načinom na koji je svijet upotrebljavao njegov izum – dinamit i u toj činjenici leži razlog njegove odluke da se nagrade dodjeljuju onima koji svojim sposobnostima najviše doprinose čovječanstvu.'
+	),
+	array(
+		'question' => 'Ko je režirao filmove “Ralje” (Jaws), “Schindlerova lista” (Schindler’s List) i “Jurski park’ Jurassic Park?',
+		'potentialAnswers' => ['Steven Spielberg', 'Stanley Kubrick', 'James Cameron', 'Alfred Hitchcock'],
+		'indexOfCorrectAnswer' => '0',
+		'descriptionOfCorrectAnswer' => 'Steven Spielberg je američki filmski redatelj, trostruki dobitnik Oscara i financijski najuspješniji filmaš u istoriji. Njegova 3 filma: Ralje, E.T. i Jurski park postali su financijski najuspješniji filmovi u svojem vremenu. Schindlerova lista je dobitnik 7 Oscara, 7 BAFTA i 3 Zlatna globusa.'
+	),
+	array(
+		'question' => 'Ko je napisao naučno-fantastični roman “Nineteen Eighty-Four (Hiljadu devetsto osamdeset četvrta, ili 1984)”?',
+		'potentialAnswers' => [ 'Mark Twain', 'George Orwell', 'Benjamin Disraeli',  'Charles Dickens'],
+		'indexOfCorrectAnswer' => '1',
+		'descriptionOfCorrectAnswer' => 'Nineteen Eighty-Four (1984) je naučno-fantastični roman koga je 1949. godine objavio britanski književnik George Orwell. 1984 je postao ne samo najpoznatiji Orwellov roman, nego jedno od klasičnih djela distopije i jedan od najuticajnijih romana 20. vijeka. Često se smatra jednim od klasika moderne književnosti.'
+	),
+	array(
+		'question' => 'Koji latinski izraz znači veliki rad, i odnosi se na najbolje, najpopularnije, ili najpriznatije postignuće autora, umjetnika ili kompozitora?',
+		'potentialAnswers' => ['Tempori parce', 'Labor omnia vincit', 'Fama volat', 'Magnum opus'],
+		'indexOfCorrectAnswer' => '3',
+		'descriptionOfCorrectAnswer' => 'Magnum opus (negdje i Opus magnum, plural magna opera), u latinskom jezikom znači veliki rad, i odnosi se na najbolje, najpopularnije, ili najpriznatije postignuće autora, umjetnika ili kompozitora, ili najčešće nešto što podrazumijeva nečiji udio ili doprinos velikom količinom materijala.'
+	),
+	array(
+		'question' => 'Ko je naslikao remek-djelo “Guernica”, vječiti podsjetnik ratnih tragedija, antiratni simbol i oličenje mira?',
+		'potentialAnswers' => ['Diego Rivera', 'Pablo Picasso', 'Salvador Dali', 'Rembrandt'],
+		'indexOfCorrectAnswer' => '1',
+		'descriptionOfCorrectAnswer' => 'Guernica je slika Pabla Picassa iz 1937. god. koja je inspirisana događajem iz Španjolskog građanskog rata; remek-djelo koje je izlagano širom svijeta, postajući poznata i široko prihvaćeno kao vječiti podsjetnik ratnih tragedija, antiratni simbol i oličenje mira. Slika je 19 godina putovala svijetom te postala “platno o kojem se najviše govorilo”.'
+	),
+	array(
+		'question' => 'Koliko krugova Pakla ima u Danteovoj Božanstvenoj komediji?',
+		'potentialAnswers' => ['Dvanaest', 'Devet', 'Sedam', 'Trinaest'],
+		'indexOfCorrectAnswer' => '1',
+		'descriptionOfCorrectAnswer' => 'Božanstvena komedija je epski spjev koji je napisao Dante Alighieri između 1304. i svoje smrti 1321. godine. Na površini, poema opisuje Danteova putovanja kroz pakao, čistilište i raj; ali na dubljem nivou ona predstavlja, alegorijski, putovanje duše prema Bogu. Pakao je alegorijska slika zemaljskoga svijeta i njegove pokvarenosti, a Dante je simbol grešnog čovjeka. Kroz Pakao i Čistilište ga vodi Vergilije, koji je njegov uzor i simbol razuma. Pakao ima 9 krugova u obliku lijevka.'
+	),
+	array(
+		'question' => 'Machu Picchu je sveti grad',
+		'potentialAnswers' => [ 'Maya', 'Azteca', 'Tolteca', 'Inka'],
+		'indexOfCorrectAnswer' => '3',
+		'descriptionOfCorrectAnswer' => 'Machu Picchu je sveti grad Inka (“Izgubljeni grad Inka”) smješten na najvišem dijelu istočnih Andi u Peruu. Machu Picchu je 2007. godine izabran kao jedno od Sedam novih svjetskih čuda.'
+	) 
+);
