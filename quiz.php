ab3b58ea159b393a037303c9ffe19653e36575fa
Nenad Bojkovic <nenad.bojkovic@uapdc.local>
2018-03-16 15:13:58 +0100

    fixing ommited radio btn

--- quiz.php
+++ quiz.php
@@ -1,31 +1,38 @@
 <?php 
-	var_dump($_POST);
+var_dump($_POST);
 
-	$quizResult = 0;
-	$maxNumberOfQuestions = 5;
-	$currentQuestionNumber = $_POST ? $_POST['questionNumber'] : NULL;
-	if($_POST){
-		session_start();
-		
-		if(isset($_POST['logout'])){
-			$_SESSION = array();
-			if(isset($_COOKIE[session_name()])){
-				setcookie(session_name(), '', time() -86400, '/');
-			}
-			session_destroy();
-			header('Location: http://localhost/quiz.php');
+
+
+$quizResult = 0;
+$maxNumberOfQuestions = 5;
+$currentQuestionNumber = $_POST ? $_POST['questionNumber'] : NULL;
+/*IF we got data through HTML form*/
+if($_POST){
+	session_start();
+	var_dump($_SESSION);
+	/*Check if the last page is reached and logout Btn pressed */
+	if(isset($_POST['logout'])){
+		$_SESSION = array();
+		if(isset($_COOKIE[session_name()])){
+			setcookie(session_name(), '', time() -86400, '/');
 		}
+		session_destroy();
+		header('Location: http://localhost/quiz/quiz.php');
+	} else{
 		require 'quizQuestions.php';
+		/*Check if the first page is reached and start Btn pressed */
 		if(isset($_POST['startBtn'])){
+			/*Generate 5 random index from questions array and save it to SESSION variable questionIndexArr*/
 			$_SESSION['questionIndexArr'] = array_rand( $questions, $maxNumberOfQuestions );
 			shuffle($_SESSION['questionIndexArr']);
 			//var_dump($_SESSION);
+			/*Pull off questions from array*/
 			foreach ($_SESSION['questionIndexArr'] as $key => $value) {
 				$tempQuestionArr[] = $questions[$value];
 			}
 			$_SESSION['userAnswerArr'] = array();
-		}
-		if(isset($_POST['questionNumber'])) {
+		} elseif(isset($_POST['questionNumber'])) {
+			/*Again same thing could it be done in some smarter way*/
 			$tempQuestionArr = [];
 			foreach ($_SESSION['questionIndexArr'] as $key => $value) {
 				$tempQuestionArr[] = $questions[$value];
@@ -39,24 +46,23 @@
 					$_SESSION['result'] = !isset($_SESSION['result']) ? 1 : $_SESSION['result']+1;
 					$quizResult = $_SESSION['result'] * 1;
 					//setcookie('result',$quizResult);
-					echo $quizResult . '<br>';
+					//echo $quizResult . '<br>';
 					echo calculateScore($tempQuestionArr, $_SESSION['userAnswerArr']) . '<br>';
 				}
+			} else {
+				//header('Location: http://localhost/quiz/quiz.php');
 			}
 		}
 	}
-	if($_GET){
-		//header('Content-Type: application/json');
-		//exit('s');
-		echo "string";
+}
+
+function calculateScore($questionsArr, $userInfoArr){
+	$score = 0;
+	for($i = 0; $i < count($userInfoArr); $i++){
+		if($questionsArr[$i]['indexOfCorrectAnswer'] == $userInfoArr[$i]['indexOfAnswer']){$score++;}
 	}
-	function calculateScore($questionsArr, $userInfoArr){
-		$score = 0;
-		for($i = 0; $i < count($userInfoArr); $i++){
-			if($questionsArr[$i]['indexOfCorrectAnswer'] == $userInfoArr[$i]['indexOfAnswer']){$score++;}
-		}
-		return $score;
-	}
+	return $score;
+}
 
 
 ?>
@@ -70,36 +76,36 @@
 		padding: 0;
 		margin: 0;
 	}
-	</style>
+</style>
 </head>
 <body>
 	<?php if(!$_POST){ ?>
 	<h1>Welcome to our Quiz</h1>
 	<form action="" method="POST">
-		<input type="hidden" id="questionNumber" name="questionNumber" value="-1">
+		<input type="hidden" id="questionNumber" name="questionNumber" value="0">
 		<input type="submit" name="startBtn" id="startBtn" value="startBtn">
 	</form>
 	<?php } elseif (isset($_POST['startBtn']) || $currentQuestionNumber < $maxNumberOfQuestions) { ?> 
-		<h3>Question Number: <?php echo ($currentQuestionNumber + 1)?></h3>
-		<p>
-			<?php echo  $tempQuestionArr[$currentQuestionNumber]['question']?>		
-		</p>
-		<form id="questionForm" action="" method="POST">
-			<ul>
-				<?php foreach ($tempQuestionArr[$currentQuestionNumber]['potentialAnswers'] as $key => $value) {
-					echo "<li><input type='radio' name='question' value=$key>" . $value . "</li>";
-				} ?>		
-			</ul>
-			<input type="hidden" id="questionNumber" name="questionNumber" value=<?php echo $_POST['questionNumber']; ?>>
-			<input type='submit' name='nextQuestionBtn' id='nextQuestionBtn' value='Next Question'/>
-		</form>
-		<button type="button" id="checkAnswerBtn">Check answer</button>
+	<h3>Question Number: <?php echo ($currentQuestionNumber + 1)?></h3>
+	<p>
+		<?php echo  $tempQuestionArr[$currentQuestionNumber]['question']?>		
+	</p>
+	<form id="questionForm" action="" method="POST">
+		<ul>
+			<?php foreach ($tempQuestionArr[$currentQuestionNumber]['potentialAnswers'] as $key => $value) {
+				echo "<li><input type='radio' name='question' value=$key required>" . $value . "</li>";
+			} ?>		
+		</ul>
+		<input type="hidden" id="questionNumber" name="questionNumber" value=<?php echo $_POST['questionNumber']; ?>>
+		<input type='submit' name='nextQuestionBtn' id='nextQuestionBtn' value='Next Question'/>
+	</form>
+	<button type="button" id="checkAnswerBtn">Check answer</button>
 	<?php } else { ?>
-		<h3>Quiz is over</h3>
-		<h4>You scored <?php echo calculateScore($tempQuestionArr, $_SESSION['userAnswerArr']) ?> points</h4>
-		<form action="" method="POST">
-			<input name="logout" type="submit" id="logout" value="Start Quiz Again">
-		</form>
+	<h3>Quiz is over</h3>
+	<h4>You scored <?php echo calculateScore($tempQuestionArr, $_SESSION['userAnswerArr']) ?> points</h4>
+	<form action="" method="POST">
+		<input name="logout" type="submit" id="logout" value="Start Quiz Again">
+	</form>
 	<?php } ?> 
 
 	<script type="text/javascript">
@@ -108,7 +114,7 @@
 		var questionNumber = document.getElementById('questionNumber');
 		var checkAnswerBtn = document.getElementById('checkAnswerBtn');
 		
-		if(startBtn){startBtn.addEventListener('click', changeQuestionNumber(1), false);}
+		/*if(startBtn){startBtn.addEventListener('click', changeQuestionNumber(1), false);}*/
 		if(nextQuestionBtn){nextQuestionBtn.addEventListener('click', changeQuestionNumber(1), false);}
 		if(checkAnswerBtn){checkAnswerBtn.addEventListener('click', testFunction, false);}
 
@@ -116,28 +122,37 @@
 		function changeQuestionNumber(val){
 			return function(){
 				//alert(val);
-				questionNumber.value = questionNumber.value * 1 + val;
+				if(document.querySelectorAll('input[type="radio"][name="question"]:checked').length > 0 ){
+							questionNumber.value = questionNumber.value * 1 + val;
+				}
 				//document.cookie = "result=0";
 				//alert(questionNumber);
-			};
+			}
 		}
-		function testFunction(){
-			var xhttp = new XMLHttpRequest();
-			//console.log(document.querySelector('input[name="question"]:checked').value);
 
-			//should check for NULL value in case that no radio button is selected
-			var answerIndex = document.querySelector('input[name="question"]:checked').value;
-			
-			  xhttp.onreadystatechange = function() {
-			    if (this.readyState == 4 && this.status == 200) {
-			    //console.log(JSON.parse(this.responseText));
-			    console.log((this.responseText));
-			     //document.getElementById("demo").innerHTML = this.responseText;
-			    }
-			  };
-			  xhttp.open("POST", "http://localhost/quiz/testPage.php", true);
-			  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-			  xhttp.send("currentQuestion=" + questionNumber.value + "&answerIndex=" + answerIndex);
+
+		
+		function testFunction(){
+			if(document.querySelectorAll('input[type="radio"][name="question"]:checked').length > 0 ){
+				var xhttp = new XMLHttpRequest();
+				//console.log(document.querySelector('input[name="question"]:checked').value);
+
+				//should check for NULL value in case that no radio button is selected
+				var answerIndex = document.querySelector('input[name="question"]:checked').value;
+				
+				xhttp.onreadystatechange = function() {
+					if (this.readyState == 4 && this.status == 200) {
+				    //console.log(JSON.parse(this.responseText));
+				    console.log((this.responseText));
+				     //document.getElementById("demo").innerHTML = this.responseText;
+				 }
+				};
+				xhttp.open("POST", "http://localhost/quiz/testPage.php", true);
+				xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+				xhttp.send("currentQuestion=" + questionNumber.value + "&answerIndex=" + answerIndex);
+			} else {
+				console.log('change this');
+			}
 		}
 
 
